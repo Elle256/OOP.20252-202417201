c1683ebc611ddac7672d69f92abaacb5edf19a4d
Elle256 <thanhthutran2006@gmail.com>
2026-03-06 20:46:48 +0700

    Implement QuickSort instead of Arrays.sort in Exercise6_5

--- Homework02/Exercise6_5.java
+++ Homework02/Exercise6_5.java
@@ -1,6 +1,29 @@
 import java.util.Scanner;
 import java.util.Arrays;
 public class Exercise6_5 {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    public static int partition(int[] arr,int low,int high){
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
     public static void main(String[] args) {
      Scanner scan = new Scanner(System.in);
       System.out.print("Enter the number of elements in the array:");
@@ -10,7 +33,7 @@ public class Exercise6_5 {
         for (int i = 0; i < n; i++) {
             arr[i] = scan.nextInt();
         }
-        Arrays.sort(arr);
+        quickSort(arr, 0, n - 1);
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += arr[i];
